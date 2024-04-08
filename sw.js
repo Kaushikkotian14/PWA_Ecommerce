cc4b6336cf857b256d8e15e6e5026650f244dee3
Kaushikkotian14 <113770540+Kaushikkotian14@users.noreply.github.com>
2024-04-08 19:01:19 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -13,43 +13,86 @@ const staticAssets = [
     './sneakers.html',
     './manifest.json',
     './style.css',
+    // Add other assets you want to cache
 ];
 
-self.addEventListener('install',async e =>{
+// Install event - cache static assets
+self.addEventListener('install', async e => {
     const cache = await caches.open(cacheName);
     await cache.addAll(staticAssets);
     return self.skipWaiting();
 });
- 
-self.addEventListener('activate',e =>{
+
+// Activate event
+self.addEventListener('activate', e => {
     self.clients.claim();
 });
 
+// Fetch event - serve assets from cache or fetch from network
 self.addEventListener('fetch', async e => {
     const req = e.request;
     const url = new URL(req.url);
   
     if (url.origin === location.origin) {
-      e.respondWith(cacheFirst(req));
+        e.respondWith(cacheFirst(req));
     } else {
-      e.respondWith(networkAndCache(req));
+        e.respondWith(networkAndCache(req));
     }
-  });
-  
-  async function cacheFirst(req) {
+});
+
+async function cacheFirst(req) {
     const cache = await caches.open(cacheName);
     const cached = await cache.match(req);
     return cached || fetch(req);
-  }
-  
-  async function networkAndCache(req) {
+}
+
+async function networkAndCache(req) {
     const cache = await caches.open(cacheName);
     try {
-      const fresh = await fetch(req);
-      await cache.put(req, fresh.clone());
-      return fresh;
+        const fresh = await fetch(req);
+        await cache.put(req, fresh.clone());
+        return fresh;
     } catch (e) {
-      const cached = await cache.match(req);
-      return cached;
+        const cached = await cache.match(req);
+        return cached;
     }
+}
+
+self.addEventListener('push', event => {
+    let data = { title: 'Hello Kaushik', body: 'You have new updates!' };
+    try {
+      if (event.data) {
+          data = event.data.json(); // Attempt to parse the incoming data as JSON
+      }
+  } catch (error) {
+      console.error('Error parsing push event data:', error);
+     
   }
+
+    const options = {
+        body: data.body,
+        icon: 'images/youtube.png',
+        badge: 'images/youtube.png'
+    };
+
+    event.waitUntil(
+        self.registration.showNotification(data.title, options)
+    );
+});
+
+
+self.addEventListener('notificationclick', event => {
+    event.notification.close();
+    
+    event.waitUntil(clients.openWindow('https://www.youtube.com/'));
+});
+
+
+self.addEventListener('sync', event => {
+    if (event.tag === 'your-sync-tag') {
+        event.waitUntil(
+           
+            console.log('Background sync event triggered')
+        );
+    }
+});
